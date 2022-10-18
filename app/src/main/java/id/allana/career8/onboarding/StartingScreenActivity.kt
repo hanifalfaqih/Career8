a4b4510370a0c6557fcd2d9123df58860c8200dd
maulanahanifar <maulanahanif33@gmail.com>
2022-10-18 12:26:16 +0700

    Finish feature swipe up starting screen

--- app/src/main/java/id/allana/career8/onboarding/StartingScreenActivity.kt
+++ app/src/main/java/id/allana/career8/onboarding/StartingScreenActivity.kt
@@ -1,12 +1,56 @@
 package id.allana.career8.onboarding
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 import id.allana.career8.R
 
 class StartingScreenActivity : AppCompatActivity() {
+
+    private lateinit var motionLayoutStartingScreen: MotionLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_starting_screen)
+
+        supportActionBar?.hide()
+
+        motionLayoutStartingScreen = findViewById(R.id.ml_starting_screen)
+        motionLayoutStartingScreen.addTransitionListener(object: MotionLayout.TransitionListener {
+
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                startActivity(Intent(this@StartingScreenActivity, IntroActivity::class.java))
+                finish()
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+
+            }
+
+        })
     }
+
 }
