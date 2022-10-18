4875c2ae95cb64d58ebf0575f98be590371b12a4
rifki <rifkiaditya24545@gmail.com>
2022-10-18 07:48:43 +0700

    add register layout

--- app/src/main/java/id/allana/career8/LoginActivity.kt
+++ app/src/main/java/id/allana/career8/LoginActivity.kt
@@ -1,11 +1,23 @@
 package id.allana.career8
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var toRegisterBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        toRegisterBtn = findViewById(R.id.to_register_button)
+        toRegisterBtn.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
