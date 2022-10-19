44ab5c1ccdcbd2a20f591bbd644071f1d7597d30
rifki <rifkiaditya24545@gmail.com>
2022-10-19 09:01:39 +0700

    add reset password layout

---
+++ app/src/main/java/id/allana/career8/Reset_Password_Activity.kt
@@ -0,0 +1,11 @@
+package id.allana.career8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Reset_Password_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+    }
+}
